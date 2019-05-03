1f11f08a9b3723e58aabc8e3cea6aa12ec595c17
Łukasz Gruszczyński <luk.gruszczynski83@gmail.com>
2019-05-03 20:52:55 +0200

    Updates

---
+++ precache-manifest.92d01fb80a9d6289c7b4db1e2c7ae3fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad22c8b11ccf8719f519",
+    "url": "/WheatherReact-App/static/js/runtime~main.7aad1bd4.js"
+  },
+  {
+    "revision": "2b59cf458b3085bec4b8",
+    "url": "/WheatherReact-App/static/js/main.864de791.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/WheatherReact-App/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2b59cf458b3085bec4b8",
+    "url": "/WheatherReact-App/static/css/main.f74fdf17.chunk.css"
+  },
+  {
+    "revision": "7501c1bc23de52599a6a78903d8f5bc9",
+    "url": "/WheatherReact-App/index.html"
+  }
+];
